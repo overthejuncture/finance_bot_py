0fdd9eaad666ab65146f21e8c67c0158afba0212
overthejuncture <overthejuncture@gmail.com>
2022-06-27 11:58:09 +0300

    -

--- bot/handlers/categories_actions.py
+++ bot/handlers/categories_actions.py
@@ -1,4 +1,8 @@
-from telegram import Update
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 from bot_helper_py import bot as utils
 from telegram.ext import (
     CommandHandler,
@@ -13,5 +17,13 @@ def handler(name: str):
     return CommandHandler(name, categories_actions)
 
 def categories_actions(update: Update, context: CallbackContext):
-    
+    user = User.byUpdate(update)
+    cats = user.categories.all()
+    buttons = [
+        InlineKeyboardButton(text='1', callback_data='1'),
+        InlineKeyboardButton(text='2', callback_data='2'),
+    ]
+    # reply_markup = InlineKeyboardMarkup(utils.build_menu(buttons=buttons, n_cols=1))
+    text, reply_markup = utils.list_with_keyboard(cats, lambda x: x.name)
+    update.message.reply_text(text=text, reply_markup=reply_markup)
     pass
